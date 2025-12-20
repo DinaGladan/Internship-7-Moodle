e39d50b06e67a817d5e1e07b78440165f342cabc
DinaGladan <dina.gladan@gmail.com>
2025-12-20 18:51:52 +0100

    Added MoodleDbContext

--- MoodleSystem/MoodleSystem.Domain/Persistence/Common/IUnitOfWork.cs
+++ MoodleSystem/MoodleSystem.Domain/Persistence/Common/IUnitOfWork.cs
@@ -1,7 +1,15 @@
-﻿namespace MoodleSystem.Domain.Persistence.Common
+﻿using MoodleSystem.Domain.Persistence.Courses;
+using MoodleSystem.Domain.Persistence.PrivateMessages;
+using MoodleSystem.Domain.Persistence.Users;
+
+namespace MoodleSystem.Domain.Persistence.Common
 {
     public interface IUnitOfWork
     {
+        IUserRepository Users { get; }
+        ICourseRepository Courses { get; }
+        IPrivateMessageRepository PrivateMessages { get; }
+
         Task CreateTransactionAsync();
         Task CommitAsync();
         Task RollBack();
---
+++ MoodleSystem/MoodleSystem.Infrastructure/Persistence/MoodleDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MoodleSystem.Domain.Entities;
+
+namespace MoodleSystem.Infrastructure.Persistence
+{
+    public class MoodleDbContext : DbContext
+    {
+        public MoodleDbContext(DbContextOptions<MoodleDbContext> options) : base(options) { }  // predstavlja spajanje s bazom
+
+        public DbSet<User> Users { get; set; } = null!; // predstavlja tablicu u bazi
+        public DbSet<Course> Courses { get; set; } = null!;
+        public DbSet<Announcement> Announcements { get; set; } = null!;
+        public DbSet<Material> Materials { get; set; } = null!;
+        public DbSet<PrivateMessage> PrivateMessages { get; set; } = null!;
+        public DbSet<UserCourse> UserCourses { get; set; } = null!;
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(MoodleDbContext).Assembly);
+
+            modelBuilder.HasDefaultSchema("public");
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
