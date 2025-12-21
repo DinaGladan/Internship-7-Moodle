c8229bd1de9d361791c37f83563d22c72a1088fe
DinaGladan <dina.gladan@gmail.com>
2025-12-22 00:42:59 +0100

    First migration

---
+++ MoodleSystem/MoodleSystem.Infrastructure/Database/DesignTimeDbContextFactory.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using MoodleSystem.Infrastructure.Persistence;
+
+namespace MoodleSystem.Infrastructure.Database
+{
+    public class DesignTimeDbContextFactory : IDesignTimeDbContextFactory<MoodleDbContext>
+    {
+        public MoodleDbContext CreateDbContext(string[] args)
+        {
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(Path.Combine(Directory.GetCurrentDirectory(), "..", "MoodleSystem.Console"))
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .Build();
+
+            var connectionString = configuration.GetConnectionString("MoodleDbContext");
+            var optionsBuilder = new DbContextOptionsBuilder<MoodleDbContext>();
+
+            optionsBuilder.UseNpgsql(connectionString);
+
+
+            return new MoodleDbContext(optionsBuilder.Options);
+        }
+    }
+}
---
+++ MoodleSystem/MoodleSystem.Infrastructure/Migrations/20251221225232_first_migration.cs
@@ -0,0 +1,238 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace MoodleSystem.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class first_migration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "public");
+
+            migrationBuilder.CreateTable(
+                name: "users",
+                schema: "public",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    first_name = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    last_name = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    email = table.Column<string>(type: "character varying(255)", maxLength: 255, nullable: false),
+                    password_hash = table.Column<string>(type: "character varying(256)", maxLength: 256, nullable: false),
+                    role = table.Column<int>(type: "integer", nullable: false),
+                    created_at = table.Column<DateTime>(type: "timestamp with time zone", nullable: false, defaultValueSql: "now()")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_users", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "courses",
+                schema: "public",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    name = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    professor_id = table.Column<int>(type: "integer", nullable: false),
+                    created_at = table.Column<DateTime>(type: "timestamp with time zone", nullable: false, defaultValueSql: "now()")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_courses", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_courses_users_professor_id",
+                        column: x => x.professor_id,
+                        principalSchema: "public",
+                        principalTable: "users",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "private_messages",
+                schema: "public",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    content = table.Column<string>(type: "character varying(1000)", maxLength: 1000, nullable: false),
+                    sender_id = table.Column<int>(type: "integer", nullable: false),
+                    receiver_id = table.Column<int>(type: "integer", nullable: false),
+                    created_at = table.Column<DateTime>(type: "timestamp with time zone", nullable: false, defaultValueSql: "now()")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_private_messages", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_private_messages_users_receiver_id",
+                        column: x => x.receiver_id,
+                        principalSchema: "public",
+                        principalTable: "users",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_private_messages_users_sender_id",
+                        column: x => x.sender_id,
+                        principalSchema: "public",
+                        principalTable: "users",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "announcements",
+                schema: "public",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    title = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    content = table.Column<string>(type: "character varying(1000)", maxLength: 1000, nullable: false),
+                    course_id = table.Column<int>(type: "integer", nullable: false),
+                    created_at = table.Column<DateTime>(type: "timestamp with time zone", nullable: false, defaultValueSql: "now()")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_announcements", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_announcements_courses_course_id",
+                        column: x => x.course_id,
+                        principalSchema: "public",
+                        principalTable: "courses",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "materials",
+                schema: "public",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    name = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    course_id = table.Column<int>(type: "integer", nullable: false),
+                    url = table.Column<string>(type: "text", nullable: false),
+                    created_at = table.Column<DateTime>(type: "timestamp with time zone", nullable: false, defaultValueSql: "now()")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_materials", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_materials_courses_course_id",
+                        column: x => x.course_id,
+                        principalSchema: "public",
+                        principalTable: "courses",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "user_courses",
+                schema: "public",
+                columns: table => new
+                {
+                    course_id = table.Column<int>(type: "integer", nullable: false),
+                    user_id = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_user_courses", x => new { x.user_id, x.course_id });
+                    table.ForeignKey(
+                        name: "FK_user_courses_courses_course_id",
+                        column: x => x.course_id,
+                        principalSchema: "public",
+                        principalTable: "courses",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_user_courses_users_user_id",
+                        column: x => x.user_id,
+                        principalSchema: "public",
+                        principalTable: "users",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_announcements_course_id",
+                schema: "public",
+                table: "announcements",
+                column: "course_id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_courses_professor_id",
+                schema: "public",
+                table: "courses",
+                column: "professor_id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_materials_course_id",
+                schema: "public",
+                table: "materials",
+                column: "course_id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_private_messages_receiver_id",
+                schema: "public",
+                table: "private_messages",
+                column: "receiver_id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_private_messages_sender_id",
+                schema: "public",
+                table: "private_messages",
+                column: "sender_id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_user_courses_course_id",
+                schema: "public",
+                table: "user_courses",
+                column: "course_id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_users_email",
+                schema: "public",
+                table: "users",
+                column: "email",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "announcements",
+                schema: "public");
+
+            migrationBuilder.DropTable(
+                name: "materials",
+                schema: "public");
+
+            migrationBuilder.DropTable(
+                name: "private_messages",
+                schema: "public");
+
+            migrationBuilder.DropTable(
+                name: "user_courses",
+                schema: "public");
+
+            migrationBuilder.DropTable(
+                name: "courses",
+                schema: "public");
+
+            migrationBuilder.DropTable(
+                name: "users",
+                schema: "public");
+        }
+    }
+}
