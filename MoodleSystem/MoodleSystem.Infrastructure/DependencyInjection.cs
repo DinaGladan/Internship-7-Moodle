cd436b1e077831ff7580ef81b7a5746ae7bf6356
DinaGladan <dina.gladan@gmail.com>
2025-12-22 08:32:20 +0100

    Added DI

---
+++ MoodleSystem/MoodleSystem.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using MoodleSystem.Domain.Abstractions.Services;
+using MoodleSystem.Domain.Persistence.Common;
+using MoodleSystem.Domain.Persistence.Courses;
+using MoodleSystem.Domain.Persistence.PrivateMessages;
+using MoodleSystem.Domain.Persistence.Statistics;
+using MoodleSystem.Domain.Persistence.Users;
+using MoodleSystem.Infrastructure.Persistence;
+using MoodleSystem.Infrastructure.Repositories;
+using MoodleSystem.Infrastructure.Service;
+
+namespace MoodleSystem.Infrastructure
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructure(
+            this IServiceCollection services,
+            IConfiguration configuration)
+        {
+            AddDatabase(services, configuration);
+            AddRepositories(services);
+            AddServices(services);
+            return services;
+        }
+
+        private static void AddDatabase(IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<MoodleDbContext>(options =>
+                options.UseNpgsql(configuration.GetConnectionString("MoodleDbContext")));
+        }
+
+        private static void AddRepositories(IServiceCollection services)
+        {
+            services.AddScoped<IUserRepository, UserRepository>();
+            services.AddScoped<ICourseRepository, CourseRepository>();
+            services.AddScoped<IPrivateMessageRepository, PrivateMessageRepository>();
+            services.AddScoped<IStatisticRepository, StatisticRepository>();
+
+            services.AddScoped<IUnitOfWork, UnitOfWork>();
+
+        }
+
+        private static void AddServices(IServiceCollection services)
+        {
+            services.AddScoped<IPasswordHasher, PasswordHasher>();
+        }
+    }
+}
---
+++ MoodleSystem/MoodleSystem.Infrastructure/Service/PasswordHasher.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Cryptography.KeyDerivation;
+using MoodleSystem.Domain.Abstractions.Services;
+using System.Security.Cryptography;
+
+namespace MoodleSystem.Infrastructure.Service
+{
+    public class PasswordHasher : IPasswordHasher
+    {
+        private const int SaltSize = 128 / 8;
+        private const int KeySize = 256 / 8;
+        private const int Iterations = 100_000;
+
+        public string Hash(string password)
+        {
+            byte[] salt = RandomNumberGenerator.GetBytes(SaltSize);
+
+            byte[] hash = KeyDerivation.Pbkdf2(
+                password: password,
+                salt: salt,
+                prf: KeyDerivationPrf.HMACSHA256,
+                iterationCount: Iterations,
+                numBytesRequested: KeySize);
+
+            return $"{Iterations}.{Convert.ToBase64String(salt)}.{Convert.ToBase64String(hash)}";
+        }
+
+        public bool Verify(string password, string passwordHash)
+        {
+            var parts = passwordHash.Split('.', 3);
+            if (parts.Length != 3)
+                return false;
+
+            int iterations = int.Parse(parts[0]);
+            byte[] salt = Convert.FromBase64String(parts[1]);
+            byte[] storedHash = Convert.FromBase64String(parts[2]);
+            byte[] computedHash = KeyDerivation.Pbkdf2(
+                password: password,
+                salt: salt,
+                prf: KeyDerivationPrf.HMACSHA256,
+                iterationCount: iterations,
+                numBytesRequested: storedHash.Length);
+
+            return CryptographicOperations.FixedTimeEquals(storedHash, computedHash);
+
+        }
+    }
+}
