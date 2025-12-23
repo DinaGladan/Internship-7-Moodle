fe082d9309e24a04821d8265391ae2f12b581bf7
DinaGladan <dina.gladan@gmail.com>
2025-12-23 14:08:34 +0100

    Added authentication for login and register

---
+++ MoodleSystem/MoodleSystem.Application/Validation/CreateCaptcha.cs
@@ -0,0 +1,19 @@
+﻿using System.CodeDom.Compiler;
+
+namespace MoodleSystem.Application.Validation
+{
+    public class CreateCaptcha
+    {
+        public string Generate()
+        {
+            const string letters = "ABCDEFGHIJKLMNOPRSTUVZQWY";
+            const string numbers = "0123456789";
+
+            var random = new Random();
+            var letter = letters[random.Next(letters.Length)];
+            var number = numbers[random.Next(numbers.Length)];
+
+            return $"{letter}{number}{random.Next(10, 20)}";
+        }
+    }
+}
---
+++ MoodleSystem/MoodleSystem.Application/Validation/GetValidEmaill.cs
@@ -0,0 +1,14 @@
+﻿using System.Text.RegularExpressions;
+
+namespace MoodleSystem.Application.Validation
+{
+    public  class GetValidEmail
+    {
+        public static bool IsEmailValid(string email)
+        {
+            string pattern = @"^\S+@\S{2,}\.\S{3,}$";
+            return Regex.IsMatch( pattern, email);
+        }
+
+    }
+}
