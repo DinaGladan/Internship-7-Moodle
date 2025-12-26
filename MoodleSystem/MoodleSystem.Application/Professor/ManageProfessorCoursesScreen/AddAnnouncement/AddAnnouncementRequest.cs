d6b356285aab57b251da0e38b474a00dea48fabd
DinaGladan <dina.gladan@gmail.com>
2025-12-26 16:04:17 +0100

    Professor can Add Announcements

---
+++ MoodleSystem/MoodleSystem.Application/Professor/ManageProfessorCoursesScreen/AddAnnouncement/AddAnnouncementRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace MoodleSystem.Application.Professor.ManageProfessorCoursesScreen.AddAnnouncement
+{
+    public class AddAnnouncementRequest
+    {
+        public int CourseId { get; init; }
+        public string Title { get; init; } = null!;
+        public string Content { get; init; } = null!;
+    }
+}
---
+++ MoodleSystem/MoodleSystem.Application/Professor/ManageProfessorCoursesScreen/AddAnnouncement/AddAnnouncementRequestHandler.cs
@@ -0,0 +1,58 @@
+﻿using MoodleSystem.Application.Common.Model;
+using MoodleSystem.Domain.Entities;
+using MoodleSystem.Domain.Persistence.Common;
+using MoodleSystem.Domain.Persistence.Courses;
+
+namespace MoodleSystem.Application.Professor.ManageProfessorCoursesScreen.AddAnnouncement
+{
+    public class AddAnnouncementRequestHandler
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly ICourseRepository _courseRepository;
+
+        public AddAnnouncementRequestHandler(IUnitOfWork unitOfWork, ICourseRepository courseRepository)
+        {
+            _unitOfWork = unitOfWork;
+            _courseRepository = courseRepository;
+        }
+
+        public async Task<AddAnnouncementResponse> AnnouncementHandler(AddAnnouncementRequest req)
+        {
+            var professorId = CurrentUser.User!.Id;
+            var course = await _courseRepository.GetByIdAsync(req.CourseId);
+
+            if(course == null)
+            {
+                return new AddAnnouncementResponse
+                {
+                    Message = "Taj kolegij ne postoji",
+                    Success = false
+                };
+            }
+
+            if(course.ProfessorId != professorId)
+            {
+                return new AddAnnouncementResponse
+                {
+                    Message = "Nemate pravo objavljivat za taj kolegij",
+                    Success = false
+                };
+            }
+
+            var announcement = new Announcement(
+                title: req.Title,
+                content: req.Content,
+                courseId: req.CourseId
+            );
+
+            course.Announcements.Add( announcement );
+            await _unitOfWork.SaveChangesAsync();
+
+            return new AddAnnouncementResponse
+            {
+                Success = true
+            };
+        }
+        
+    }
+}
