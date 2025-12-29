ab3bfa3bffeb32a958a0a5ebcc5ed142dea86442
DinaGladan <dina.gladan@gmail.com>
2025-12-29 12:20:32 +0100

    added basic reader and writer

--- MoodleSystem/MoodleSystem.Console/Helpers/Reader.cs
+++ MoodleSystem/MoodleSystem.Console/Helpers/Reader.cs
@@ -1,12 +1,35 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace MoodleSystem.Console.Helpers
+﻿namespace MoodleSystem.Console.Helpers
 {
-    internal class Reader
+    public class Reader
     {
+        public static string ReadLine()
+        {
+            return System.Console.ReadLine() ?? "";
+        }
+
+        public static string ReadInput(string prompt)
+        {
+            System.Console.Write(prompt);
+            return System.Console.ReadLine() ?? "";
+        }
+
+        public static int? ReadInt(string prompt)
+        {
+            System.Console.Write(prompt);
+            var input = System.Console.ReadLine();
+
+            if (int.TryParse(input, out int result))
+            {
+                return result;
+            }
+
+            return null;
+        }
+
+        public static string ReadMenuChoice(string prompt = "Odaberi opciju: ")
+        {
+            System.Console.Write(prompt);
+            return System.Console.ReadLine() ?? "";
+        }
     }
 }
--- MoodleSystem/MoodleSystem.Console/Helpers/Writer.cs
+++ MoodleSystem/MoodleSystem.Console/Helpers/Writer.cs
@@ -2,6 +2,43 @@
 {
     public class Writer
     {
+        public static void DisplayMenu(string title, Dictionary<string, (string Description, Func<Task<bool>> Action)> options)
+        {
+            System.Console.WriteLine($"\n=== {title} ===");
 
+            foreach (var option in options)
+            {
+                System.Console.WriteLine($"{option.Key}. {option.Value.Description}");
+            }
+        }
+        public static void WriteMessage(string message)
+        {
+            System.Console.WriteLine(message);
+        }
+
+        public static void WaitForKey()
+        {
+            System.Console.WriteLine("Pritisnite bilo koju tipku za nastavak...");
+            System.Console.ReadKey();
+        }
+
+        public static void WriteList(IEnumerable<string> items)
+        {
+            var list = items.ToList();
+
+            if (!list.Any())
+            {
+                System.Console.WriteLine("Nema podataka");
+                return;
+            }
+
+            for(int i = 0;i<list.Count; i++)
+            {
+                System.Console.WriteLine($"{i + 1}. {list[i]}");
+            }
+        }
     }
 }
+
+
+
